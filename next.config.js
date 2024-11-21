7aaa86470ed3e74670237065b886ff12fc85ce31
Daniel Lopes <danielvlopes@gmail.com>
2024-11-20 19:45:41 -0800

    Source map in production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Optional: Enforces best practices for React
-  swcMinify: true, // Uses SWC for faster builds
-  outputFileTracing: true, // Ensures serverless deployment compatibility
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  outputFileTracing: true,
 };
 
 module.exports = nextConfig;
