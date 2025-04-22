a681cd9a8e5ce73537a42783b6455978c94f3dcb
Edin Abazi <edinchezz@gmail.com>
2025-04-22 17:43:54 +0200

    Finish decorations for the demo booking page testimonials sidebar

--- app/book-demo/page.tsx
+++ app/book-demo/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import { useEffect, useState, useRef } from 'react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Quotes from '@/components/icons/Quotes';
+import DotPatternBackground from '@/components/ui/DotPatternBackground';
+import useResponsiveDevice from '@/hooks/useResponsiveDevice';
+import Image from 'next/image';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import Image from 'next/image';
 
 const reviewsData = [
   {
@@ -38,14 +42,18 @@ const reviewsData = [
 ];
 
 const allLogos = [
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/Deepgram_logo_ca172c4fe3.svg', alt: 'Deepgram', height: 20 },
+  { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_webflow_b7ad975616.svg', alt: 'Webflow', height: 19 },
+  { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_reddit_white_ebfbb73438.svg', alt: 'Reddit', height: 27 },
   { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/Ramp_idx_W_Yq9_Eu_0_1e7f07ca5f.svg', alt: 'Ramp', height: 20 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/metadata_4323b9c4fd.svg', alt: 'Metadata', height: 20 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/service_titan_fbd8ccb6f1.svg', alt: 'Service Titan', height: 20 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/swoogo_logo_449d7f6b4c.svg', alt: 'Swoogo', height: 20 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/steadily_logo_2_a9fb3f3b54.svg', alt: 'Steadily', height: 15 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/reddit_logo_b80d54a7ab.svg', alt: 'Reddit', height: 26 },
-  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/webstacks_5f9bb33e5c.svg', alt: 'Webstacks', height: 20 },
+  { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_strapi_white_0bac569aa4.svg', alt: 'Strapi', height: 20 },
+  { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_gc_ai_3258a7e45d.svg', alt: 'GC AI', height: 16 },
+  { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_airbyte_40413c6ff7.svg', alt: 'Airbyte', height: 26 },
+  // { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_shaped_2b0e8914ef.svg', alt: 'Shaped', height: 24 },
+  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/webstacks_5f9bb33e5c.svg', alt: 'Webstacks', height: 17 },
+  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/Deepgram_logo_ca172c4fe3.svg', alt: 'Deepgram', height: 22 },
+  { src: 'https://growthxlabs-prod-strapi-bucket.s3.us-east-1.amazonaws.com/service_titan_fbd8ccb6f1.svg', alt: 'Service Titan', height: 25 },
+  // { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_abnormal_be02983bea.svg', alt: 'Abnormal', height: 16 },
+  // { src: 'https://glowing-rainbow-627a62133d.media.strapiapp.com/logo_gitpod_2e24380a04.svg', alt: 'Gitpod', height: 26 },
 ];
 
 export default function BookDemoPage() {
@@ -53,6 +61,7 @@ export default function BookDemoPage() {
   const [hasError, setHasError] = useState(false);
 
   const paginationRef = useRef(null);
+  const { isDesktop } = useResponsiveDevice();
 
   useEffect(() => {
     document.body.classList.add('hide-nav');
@@ -113,16 +122,22 @@ export default function BookDemoPage() {
           />
         )}
       </div>
-      <div className="hidden lg:flex lg:w-1/2 h-full flex-col justify-center items-center bg-ui-black text-white">
+      <div className="hidden lg:flex lg:w-1/2 h-full flex-col justify-center items-center bg-ui-black text-white pt-[15dvh] overflow-hidden relative">
+        <img src="/demo-line-decoration.svg" alt="Line Decortion" className="absolute top-0 left-0 min-w-[70%] max-w-[90%] max-h-[40dvh] select-none pointer-events-none" aria-hidden="true" />
+        <div className="absolute top-1/2 left-1/2 translate-x-[100%] -translate-y-[80%] w-[160px] h-[520px] pt-2 bg-gradient-to-b from-[#33FF9D] from-[21.19%] to-[#20233A] to-[56.51%] select-none pointer-events-none" aria-hidden="true">
+          <img src="/balloon.png" alt="Balloon Decoration" className="w-full h-auto select-none pointer-events-none" aria-hidden="true" />
+        </div>
+        <div className="demo-dotted-pattern absolute top-[18dvh] left-[3dvh] w-[25dvh] h-[35dvh]"></div>
         <div className="flex flex-col items-center justify-center bg-[rgba(247,244,255,0.7)] p-3 rounded-2xl relative">
           <Swiper
             // install Swiper modules
-            modules={[Navigation, Pagination, A11y]}
+            modules={[Navigation, Pagination, A11y, Autoplay]}
             spaceBetween={0}
             loop={true}
             slidesPerView={1}
             navigation={false}
             pagination={{ clickable: true, el: paginationRef.current }}
+            autoplay={{ delay: 4000, disableOnInteraction: false }}
             onSwiper={(swiper) => {
               if (swiper.params.pagination && typeof swiper.params.pagination !== 'boolean') {
                 swiper.params.pagination.el = paginationRef.current;
@@ -131,10 +146,10 @@ export default function BookDemoPage() {
                 swiper.pagination.update();
               }
             }}
-            className="max-w-[420px] h-[340px] bg-white rounded-2xl"
+            className="max-w-[420px] h-[320px] bg-white rounded-2xl"
           >
             {reviewsData.map((review, index) => (
-              <SwiperSlide key={index} className="text-black font-elza text-lg cursor-grab active:cursor-grabbing p-12">
+              <SwiperSlide key={index} className="text-black font-elza text-lg cursor-grab active:cursor-grabbing px-12 py-8">
                 <div className="h-full flex flex-col">
                   <div className="flex-grow">
                     <Quotes className="text-ui-black mb-4" />
@@ -146,7 +161,7 @@ export default function BookDemoPage() {
                     </div>
                     <div className="flex flex-col ml-2 gap-0.5">
                       <p className="text-base font-medium">{review.name}</p>
-                      <p className="text-sm -mt-1">{review.legend}</p>
+                      <p className="text-sm -mt-1 opacity-75">{review.legend}</p>
                     </div>
                   </div>
                 </div>
@@ -162,11 +177,17 @@ export default function BookDemoPage() {
             ></div>
           ))}
         </div>
-        <div className="flex flex-col items-center justify-center mt-12 p-8">
-          <p className="text-sm">Trusted by</p>
-          <div className="flex flex-row flex-wrap items-center justify-center gap-4">
+        <div className="flex flex-col gap-8 items-center justify-center mt-16 max-w-[600px] mx-auto px-4">
+          <p className="text-sm text-white/75">Trusted by</p>
+          <div className="flex flex-row flex-wrap items-center justify-center gap-8">
             {allLogos.map((logo, index) => (
-              <img src={logo.src} alt={logo.alt} key={index} className="w-auto brightness-0 invert" style={{ height: logo.height }} />
+              <img 
+                src={logo.src} 
+                alt={logo.alt} 
+                key={index} 
+                className={`w-auto ${logo.alt !== 'Reddit' && logo.alt !== 'Strapi' ? 'brightness-0 invert' : ''}`} 
+                style={{ height: logo.height }} 
+              />
             ))}
           </div>
         </div>
