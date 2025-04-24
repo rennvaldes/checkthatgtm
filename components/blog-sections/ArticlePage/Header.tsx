e17a67f05fcd6c49b6a81abe54001d65f997d87d
federico avelin <favelin@litebox.ai>
2025-04-24 10:00:51 -0300

    refactor: update styles header, show new shared buttons and article image

--- components/blog-sections/ArticlePage/Header.tsx
+++ components/blog-sections/ArticlePage/Header.tsx
@@ -1,14 +1,46 @@
 /* eslint-disable @next/next/no-img-element */
 import ArrowRight from '@/components/icons/ArrowRight';
+import Instagram from '@/components/icons/Instagram';
+import Linkedin from '@/components/icons/Linkedin';
+import LinkIcon from '@/components/icons/LinkIcon';
+import Twitter from '@/components/icons/Twitter';
 import useGetQueryWithRefetchOnChange from '@/hooks/useGetQueryWithRefetchOnChange';
 import { getMainDataAndArticles } from '@/lib/api/strapi/blog';
 import { Skeleton } from '@/lib/shadcn/ui/skeleton';
 import Link from 'next/link';
-import React from 'react';
+import React, { ComponentProps } from 'react';
 
+function ShareLink({ children, ...props }: ComponentProps<'a'>) {
+  return (
+    <a
+      {...props}
+      className='hover:bg-ui-green-light transition-color rounded-full p-[16px] duration-300 cursor-pointer'
+      target='_blank'>
+      {children}
+    </a>
+  );
+}
+
+function ArticleImage({ imageUrl, isLoading }: { imageUrl: string; isLoading: boolean }) {
+  if (isLoading) {
+    return <Skeleton className='w-full h-[186px] lg:h-[461px] mt-10 order-5 md:order-1' />;
+  }
+
+  if (!imageUrl) {
+    return null;
+  }
+
+  return <img className='pt-10 order-5 md:order-1' src={imageUrl} alt='article-image' />;
+}
+
+function getFormattedDate(date: string) {
+  const formattedDate = new Date(date);
+
+  return `${formattedDate.getDay()} ${new Intl.DateTimeFormat('en-US', { month: 'short', year: 'numeric' }).format(formattedDate)}`;
+}
 
 function BlogPageHeader({ data, isLoading }: { isLoading: boolean; data: any }) {
-  const { category, title, publisher_name, publisher_avatar, publisher_legend } = data;
+  const { category, image, title, publisher_name, publisher_avatar, publisher_legend, updatedAt } = data;
   const categoryName = category?.name || category;
 
   const { data: rawData, isLoading: isGeneralDataLoading } = useGetQueryWithRefetchOnChange({
@@ -16,40 +48,24 @@ function BlogPageHeader({ data, isLoading }: { isLoading: boolean; data: any }) 
     getFn: () => getMainDataAndArticles(),
   });
 
-  const { blog_x_link, blog_instagram_link, blog_linkedin_link } = React.useMemo(
-    () => rawData?.data.attributes ?? {},
-    [rawData]
-  );
+  const { blog_x_link, blog_instagram_link, blog_linkedin_link } = React.useMemo(() => rawData?.data ?? {}, [rawData]);
 
   return (
-    <section className='flex w-full max-w-[1280px] flex-col items-start self-center px-[20px] pb-[4px] lg:px-0 lg:pb-0'>
+    <section className='flex w-full max-w-[624px] transition-[max-width] lg:max-w-[863px] flex-col items-start self-center px-[20px] pb-[4px] lg:px-0 lg:pb-0'>
       <Link href='/blog' className='flex items-center justify-center gap-[12px] py-[12px] font-medium hover:underline'>
         <ArrowRight className='scale-x-[-1]' />
         Back to blog
       </Link>
 
-      {isLoading ? (
-        <Skeleton className='bg-ui-black/50 mt-[32px] h-[28px] w-[112px] rounded-full lg:mt-[40px]' />
-      ) : (
-        <div className='mt-[32px] rounded-full bg-[#DEDEF0] px-[12px] py-[8px] text-[12px] font-medium lg:mt-[40px]'>
-          {categoryName}
-        </div>
-      )}
-      {isLoading ? (
-        <Skeleton className='mt-[16px] h-[62px] w-full lg:mt-[16px] lg:h-[114px] lg:w-[850px]' />
-      ) : (
-        <h1 className='mt-[16px] text-[28px] font-medium leading-[31px] lg:mt-[16px] lg:w-[850px] lg:text-[52px] lg:leading-[57px]'>
-          {title}
-        </h1>
-      )}
+      <ArticleImage imageUrl={image} isLoading={isLoading} />
 
-      <div className='mt-[20px] flex w-full flex-col lg:mt-[40px] lg:flex-row lg:items-center lg:justify-between'>
+      <div className='mt-[20px] flex flex-col w-full lg:mt-[40px] md:flex-row md:items-center md:justify-between order-3 md:order-1'>
         <div className='flex items-center gap-[12px] lg:items-stretch'>
           {isLoading ? (
             <Skeleton className='h-[32px] w-[32px] rounded-full lg:h-[44px] lg:w-[44px]' />
           ) : (
             <img
-              className='h-[32px] w-[32px] rounded-full lg:h-[44px] lg:w-[44px]'
+              className='h-[32px] w-[32px] rounded-full lg:h-[44px] lg:w-[44px] bg-ui-peach'
               alt='placeholder-user'
               src={publisher_avatar}
             />
@@ -57,11 +73,11 @@ function BlogPageHeader({ data, isLoading }: { isLoading: boolean; data: any }) 
           {isLoading ? (
             <Skeleton className='h-[18px] w-[103px] lg:h-[42px]' />
           ) : (
-            <div className='font-elza flex flex-col justify-center lg:leading-[21px]'>
-              <h4 className='lg:font-[600]'>
-                <span className='lg:hidden'>by</span> {publisher_name}
+            <div className='font-elza flex flex-col justify-center leading-[21px]'>
+              <h4 className='font-normal md:font-[600]'>
+                <span>by</span> {publisher_name}
               </h4>
-              {publisher_legend && <p className='hidden lg:block'>{publisher_legend}</p>}
+              {publisher_legend && <p className='hidden md:block'>{publisher_legend}</p>}
             </div>
           )}
         </div>
@@ -69,25 +85,22 @@ function BlogPageHeader({ data, isLoading }: { isLoading: boolean; data: any }) 
           <Skeleton className='mb-[8px] mt-[16px] h-[44px] lg:w-[300px]' />
         ) : (
           <div className='mt-[16px] flex items-center justify-start mb-[8px]'>
-            <p className='p-[16px] font-medium'>Share</p>
-            {/* <a
-              href={blog_linkedin_link}
-              className='hover:bg-ui-black/10 transition-color rounded-full p-[16px] duration-300'>
+            <p className='pl-0 md:p-[16px] mr-5 font-medium'>Share</p>
+
+            <ShareLink href={blog_linkedin_link}>
               <Linkedin />
-            </a>
-            <a href={blog_x_link} className='hover:bg-ui-black/10 transition-color rounded-full p-[16px] duration-300'>
+            </ShareLink>
+            <ShareLink href={blog_x_link}>
               <Twitter />
-            </a>
-            <a
-              href={blog_instagram_link}
-              className='hover:bg-ui-black/10 transition-color rounded-full p-[16px] duration-300'>
+            </ShareLink>
+            <ShareLink href={blog_instagram_link}>
               <Instagram />
-            </a> */}
+            </ShareLink>
 
             <div className='relative flex items-center justify-center gap-1'>
               <button
                 onClick={() => navigator.clipboard.writeText(location.href)}
-                className='hover:bg-ui-black/10 transition-color peer rounded-full p-[16px] duration-300'>
+                className='hover:bg-ui-green-light transition-color peer rounded-full p-[16px] duration-300'>
                 <LinkIcon />
               </button>
               <p className='invisible absolute -top-4 text-xs peer-focus:visible'>Copied!</p>
@@ -96,7 +109,23 @@ function BlogPageHeader({ data, isLoading }: { isLoading: boolean; data: any }) 
         )}
       </div>
 
-      <div className='border-ui-black w-full border-b-[1px]' />
+      <div className='border-ui-black w-full border-b-[1px] order-4 md:order-1' />
+
+      {isLoading ? (
+        <Skeleton className='bg-ui-black/50 mt-[32px] h-[28px] w-[112px] rounded-full lg:mt-[40px]' />
+      ) : (
+        <div className='flex mt-[32px] lg:mt-[40px] items-center gap-4 order-1'>
+          <div className='rounded-full bg-[#DEDEF0] px-[12px] py-[8px] text-[12px] font-medium'>{categoryName}</div>
+          <span>Updated on {getFormattedDate(updatedAt)}</span>
+        </div>
+      )}
+      {isLoading ? (
+        <Skeleton className='mt-[16px] h-[62px] w-full lg:h-[114px] lg:w-[850px]' />
+      ) : (
+        <h1 className='mt-[16px] text-[28px] font-medium leading-[31px] lg:w-[850px] lg:text-[52px] lg:leading-[57px] order-2 md:order-1'>
+          {title}
+        </h1>
+      )}
     </section>
   );
 }
