2a0b182a0483f375819eb50db3a3b89521cb12a9
federico avelin <favelin@litebox.ai>
2025-04-24 09:55:50 -0300

    refactor: move share icons to icons folder

---
+++ components/icons/Twitter.tsx
@@ -0,0 +1,14 @@
+function Twitter() {
+  return (
+    <svg width='17' height='16' viewBox='0 0 17 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M10.2277 5.52714L6.84 0.291664H1.18448L6.89995 9.12467L0.810059 15.7083H2.51282L7.60081 10.2078L11.16 15.7083H16.8155L10.9285 6.61028L16.7732 0.291664H15.0705L10.2277 5.52714ZM11.84 14.4583H14.5178L6.15997 1.54166H3.48216L11.84 14.4583Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
+
+export default Twitter;
