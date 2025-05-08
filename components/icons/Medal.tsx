0bd4d9cf6630d1c09d0d8a85dadb322fb4072b88
federico avelin <favelin@litebox.ai>
2025-05-08 17:41:21 -0300

    feat: add new assets for values about section

---
+++ components/icons/Medal.tsx
@@ -0,0 +1,30 @@
+export default function Medal() {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M7.21044 15L2.66044 7.14C2.46425 6.80087 2.37197 6.41159 2.39508 6.02048C2.4182 5.62937 2.55567 5.25366 2.79044 4.94L4.40044 2.8C4.58674 2.55161 4.82831 2.35 5.10602 2.21115C5.38373 2.07229 5.68995 2 6.00044 2H18.0004C18.3109 2 18.6172 2.07229 18.8949 2.21115C19.1726 2.35 19.4142 2.55161 19.6004 2.8L21.2004 4.94C21.4368 5.25265 21.576 5.62784 21.6009 6.01897C21.6258 6.4101 21.5352 6.79992 21.3404 7.14L16.7904 15'
+        stroke='#20233A'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path
+        d='M11.0001 12L5.12012 2.19995'
+        stroke='#20233A'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path d='M13 12L18.88 2.19995' stroke='#20233A' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      <path d='M8 7H16' stroke='#20233A' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      <path
+        d='M12 22C14.7614 22 17 19.7614 17 17C17 14.2386 14.7614 12 12 12C9.23858 12 7 14.2386 7 17C7 19.7614 9.23858 22 12 22Z'
+        stroke='#20233A'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path d='M12 18V16H11.5' stroke='#20233A' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+    </svg>
+  );
+}
