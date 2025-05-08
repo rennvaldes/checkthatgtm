af2687ad106343a1af9e3e92465ec125802c1a1d
federico avelin <favelin@litebox.ai>
2025-05-08 11:21:21 -0300

    feat: create initial about page and add hero section

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from '@/components/about-sections/HeroSection';
+import { Main } from '@/components/ui/Main';
+
+export default function About() {
+  return (
+    <Main>
+      <HeroSection />
+    </Main>
+  );
+}
