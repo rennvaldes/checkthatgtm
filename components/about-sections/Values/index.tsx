27ad11d9123aeb0d5fa4756eb1d14dca99780cd3
federico avelin <favelin@litebox.ai>
2025-05-09 17:05:50 -0300

    fix: dots overflow in values section

--- components/about-sections/Values/index.tsx
+++ components/about-sections/Values/index.tsx
@@ -3,7 +3,7 @@ import TitleAndDescription from './TitleAndDescription';
 
 export default function Values() {
   return (
-    <section className='px-4 pt-10 pb-20 bg-ui-black w-full flex flex-col lg:flex-row lg:justify-between lg:pl-20 lg:pr-[115px] lg:pt-[120px] lg:pb-[52px] lg:relative '>
+    <section className='px-4 pt-10 pb-20 bg-ui-black w-full flex flex-col lg:flex-row lg:justify-between lg:pl-20 lg:pr-[115px] lg:pt-[120px] lg:pb-[52px] lg:relative lg:overflow-clip'>
       <TitleAndDescription />
       <Cards />
     </section>
