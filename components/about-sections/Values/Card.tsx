f9a195c16249d2751b160c97489a2d18ee1d9b37
federico avelin <favelin@litebox.ai>
2025-05-12 15:00:13 -0300

    fix: opacity in card values

--- components/about-sections/Values/Card.tsx
+++ components/about-sections/Values/Card.tsx
@@ -13,7 +13,7 @@ export default function Card({ children, ...props }: Props) {
     offset: ['start end', 'end start'],
   });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.6, 0.7, 1], [0, 1, 1, 1]);
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
 
   return (
     <motion.article ref={ref} style={{ opacity: opacity }} {...props}>
