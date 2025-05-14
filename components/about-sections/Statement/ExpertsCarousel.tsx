5dd8ad65e67d0f62250d619a06f7b71e744003b6
federico avelin <favelin@litebox.ai>
2025-05-14 09:14:37 -0300

    fix: center title in experts carousel

--- components/about-sections/Statement/ExpertsCarousel.tsx
+++ components/about-sections/Statement/ExpertsCarousel.tsx
@@ -42,47 +42,49 @@ function ExpertsCarousel() {
   useEffect(handleCarouselWall, [handleCarouselWall]);
 
   return (
-    <div className='w-[calc(100%_+_16px)] lg:w-full mt-10 flex flex-col gap-6 lg:gap-10 lg:mt-20 lg:relative lg:max-w-[1920px] lg:pl-[clamp(62px,_calc((100vw_-_1440px)_/_2_+_62px),241px)]'>
+    <div className='w-[calc(100%_+_16px)] lg:w-full mt-10 flex flex-col gap-6 lg:gap-10 lg:mt-20 lg:relative'>
       <h3 className='font-clash-display font-[500] text-2xl lg:text-4xl [&&]:leading-[114%] tracking-normal text-center'>
         Meet the team
       </h3>
 
       <div className='hidden 2xl:block bg-ui-peach w-28 aspect-square rounded-full absolute -left-[56px] top-[320px] ml-[clamp(62px,_calc((100vw_-_1440px)_/_2_+_62px),241px)]' />
 
-      <Carousel
-        className='bg-ui-white'
-        setApi={setCarouselApi}
-        opts={{
-          align: 'start',
-          slidesToScroll: 1,
-          startIndex: 0,
-        }}>
-        <CarouselContent>
-          {IMAGES.map((imgSrc, index) => (
-            <CarouselItem className='basis-auto first-of-type:pl-4 lg:pl-8' key={`expert-image-${index.toString()}`}>
-              <img className='h-48 lg:h-[471px]' src={imgSrc} alt={`expert-${index}`} />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      <div className='flex flex-col gap-6 lg:gap-10 lg:max-w-[1920px] lg:pl-[clamp(62px,_calc((100vw_-_1440px)_/_2_+_62px),241px)]'>
+        <Carousel
+          className='bg-ui-white '
+          setApi={setCarouselApi}
+          opts={{
+            align: 'start',
+            slidesToScroll: 1,
+            startIndex: 0,
+          }}>
+          <CarouselContent>
+            {IMAGES.map((imgSrc, index) => (
+              <CarouselItem className='basis-auto first-of-type:pl-4 lg:pl-8' key={`expert-image-${index.toString()}`}>
+                <img className='h-48 lg:h-[471px]' src={imgSrc} alt={`expert-${index}`} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
 
-      <div className='flex w-min gap-11 lg:gap-7'>
-        <KitButton
-          variant='secondary'
-          size='custom'
-          isDisabled={carouselWall === 'left'}
-          onClick={() => carouselApi?.scrollPrev()}
-          className='disabled:text-ui-black/40 lg:px-4'>
-          <ArrowRight className='rotate-180 lg:w-8 lg:h-8' />
-        </KitButton>
-        <KitButton
-          variant='secondary'
-          size='custom'
-          isDisabled={carouselWall === 'right'}
-          onClick={() => carouselApi?.scrollNext()}
-          className='disabled:text-ui-black/40 lg:px-4'>
-          <ArrowRight className='lg:w-8 lg:h-8' />
-        </KitButton>
+        <div className='flex w-min gap-11 lg:gap-7'>
+          <KitButton
+            variant='secondary'
+            size='custom'
+            isDisabled={carouselWall === 'left'}
+            onClick={() => carouselApi?.scrollPrev()}
+            className='disabled:text-ui-black/40 lg:px-4'>
+            <ArrowRight className='rotate-180 lg:w-8 lg:h-8' />
+          </KitButton>
+          <KitButton
+            variant='secondary'
+            size='custom'
+            isDisabled={carouselWall === 'right'}
+            onClick={() => carouselApi?.scrollNext()}
+            className='disabled:text-ui-black/40 lg:px-4'>
+            <ArrowRight className='lg:w-8 lg:h-8' />
+          </KitButton>
+        </div>
       </div>
     </div>
   );
