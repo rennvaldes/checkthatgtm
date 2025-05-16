ab2ddc0a8e252f8d31f6b08a6e41beb86b46bf83
Edin Abazi <ed@razegrowth.com>
2025-05-16 12:15:50 +0200

    Fix a lot of incorrect syntax for React

--- components/about-sections/HeroSection/HeroLineDesktop.tsx
+++ components/about-sections/HeroSection/HeroLineDesktop.tsx
@@ -8,7 +8,7 @@ export default function HeroLineDesktop(props: Props) {
       <path
         d='M4 -6.10352e-05V40.5161C4 148.537 59.2969 249.039 150.542 306.855V306.855C227.403 355.558 321.557 368.682 408.807 342.853L658.337 268.985C677.26 263.383 694.894 254.108 710.232 241.689V241.689C808.452 162.16 955 232.064 955 358.445V492'
         stroke='url(#paint0_linear_1378_19713)'
-        stroke-width='8'
+        strokeWidth='8'
       />
       <defs>
         <linearGradient
@@ -18,8 +18,8 @@ export default function HeroLineDesktop(props: Props) {
           x2='890.402'
           y2='433'
           gradientUnits='userSpaceOnUse'>
-          <stop stop-color='#33FF9D' stop-opacity='0' />
-          <stop offset='1' stop-color='#33FF9D' />
+          <stop stopColor='#33FF9D' stopOpacity='0' />
+          <stop offset='1' stopColor='#33FF9D' />
         </linearGradient>
       </defs>
     </svg>
--- components/about-sections/Statement/Line.tsx
+++ components/about-sections/Statement/Line.tsx
@@ -14,7 +14,7 @@ export default function Line({ className }: Props) {
       <path
         d='M-9 560V560C-0.557768 560 6.28602 553.156 6.28602 544.714L6.28603 474.371C6.28603 427.079 44.6237 388.741 91.9155 388.741H281C336.228 388.741 381 343.969 381 288.741L381 -15'
         stroke='url(#paint0_linear_1340_8503)'
-        stroke-width='4'
+        strokeWidth='4'
       />
       <defs>
         <linearGradient
@@ -24,9 +24,9 @@ export default function Line({ className }: Props) {
           x2='433.931'
           y2='50.008'
           gradientUnits='userSpaceOnUse'>
-          <stop offset='0.121657' stop-color='#4400FF' stop-opacity='0' />
-          <stop offset='0.733733' stop-color='#4400FF' />
-          <stop offset='0.874483' stop-color='#4400FF' stop-opacity='0.1' />
+          <stop offset='0.121657' stopColor='#4400FF' stopOpacity='0' />
+          <stop offset='0.733733' stopColor='#4400FF' />
+          <stop offset='0.874483' stopColor='#4400FF' stopOpacity='0.1' />
         </linearGradient>
       </defs>
     </svg>
