38c5825af6cc8b280a293eebe59f51b663126acc
Marcus Derencius <derencius@gmail.com>
2025-05-18 19:49:46 -0700

    debug

--- lib/api/strapi/blog.ts
+++ lib/api/strapi/blog.ts
@@ -56,11 +56,7 @@ export async function getMainDataAndArticles({
   });
 }
 
-export async function getArticle(slug: string) {
-  const isLocalEnv = process.env.NEXT_PUBLIC_STRAPI_IS_LOCAL_ENV === "true";
-  const isPullRequest = process.env.NEXT_PUBLIC_IS_PULL_REQUEST === "true";
-  const showDrafts = isLocalEnv || isPullRequest;
-
+export async function getArticle(slug: string, showDrafts: boolean = false) {
   const data = await getWithQsParams("/articles", {
     filters: {
       $and: [
