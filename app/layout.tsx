bd52f5b27c0706f480759fe42818e570132ab392
n00stalgic <n0000stalgic@gmail.com>
2025-07-08 12:14:01 -0700

    feat: add LinkedIn Insight Tag to layout (script and noscript)

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,9 +74,35 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           `}
         </Script>
         {/* End Google Tag Manager */}
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight-init" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "7407172";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight-loader" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        {/* End LinkedIn Insight Tag */}
       </head>
       <body
         className={`${clashDisplay.variable} font-clash-display bg-ui-white text-ui-black text-[14px] font-normal leading-none`}>
+        {/* LinkedIn Insight Tag (noscript) */}
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} alt="" src="https://px.ads.linkedin.com/collect/?pid=7407172&fmt=gif" />
+        </noscript>
+        {/* End LinkedIn Insight Tag (noscript) */}
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KH6VJSL7"
