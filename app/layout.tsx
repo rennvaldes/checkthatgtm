8ceb58a30ea40de3896cf8c8b5add9c52be99aab
n00stalgic <n0000stalgic@gmail.com>
2025-07-08 12:07:31 -0700

    chore: clean up Google Tag Manager integration (remove unused gtm.tsx, keep only required GTM code in layout)

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,9 +63,26 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <Script id="default-form" strategy="afterInteractive">
           {`(function(w,d){w.__default__={team_id:432,form_id:828228};var s=d.createElement('script');s.async=!0;s.src='https://import-cdn.default.com/v2/index.js';d.head.appendChild(s)})(window,document);`}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KH6VJSL7');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${clashDisplay.variable} font-clash-display bg-ui-white text-ui-black text-[14px] font-normal leading-none`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KH6VJSL7"
+                  height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>
           <Navbar />
           {children}
